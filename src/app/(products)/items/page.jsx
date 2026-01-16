3998d74629d99670ecbce8a35eb24d958f85828f
polokrf <polokkumar9030@gmail.com>
2026-01-16 20:25:28 +0600

    add new function

--- src/app/(products)/items/page.jsx
+++ src/app/(products)/items/page.jsx
@@ -13,7 +13,7 @@ const Items = async () => {
       </div>
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 ">
         {result.map(product => (
-          <ItemsCard key={product._id} product={product}></ItemsCard>
+          <ItemsCard key={product._id.toString()} product={product}></ItemsCard>
         ))}
       </div>
     </div>
